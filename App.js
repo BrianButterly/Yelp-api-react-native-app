bd92e91f4bf08ed359722375712534f3d941a6b6
Brian Butterly <butterlybrian@gmail.com>
2021-02-02 15:59:30 -0600

    react navigation basic setup

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { createStackNavigator, createAppContainer } from "react-navigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Landing Page!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import SearchScreen from "./src/screens/SearchScreen";
+
+const navigator = createStackNavigator({
+  Search: SearchScreen,
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Business Search'
+  }
 });
+
+export default createAppContainer(navigator)
