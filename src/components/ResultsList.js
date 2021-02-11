3870d8e2123a4222398b2b7490538313e0e04514
Brian Butterly <butterlybrian@gmail.com>
2021-02-11 15:59:28 -0600

    results detail component added

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
+import ResultsDetail from "./ResultsDetail";
 
 const ResultsList = ({ title, results }) => {
   return (
     <View>
       <Text style={styles.titleStyle}>{title}</Text>
-      <FlatList 
+      <FlatList
         horizontal={true}
         data={results}
         keyExtractor={(result) => result.id}
         renderItem={({ item }) => {
-            return <Text>{item.name}</Text>
+          return <ResultsDetail result={item} />;
         }}
       />
     </View>
